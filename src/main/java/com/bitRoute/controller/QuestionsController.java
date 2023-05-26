348817b28b56a854204434844ff96a191b4eb87e
manali <manaliumardand04@gmail.com>
2023-05-26 10:46:43 +0530

    Added Swagger Documentation

--- src/main/java/com/bitRoute/controller/QuestionsController.java
+++ src/main/java/com/bitRoute/controller/QuestionsController.java
@@ -4,6 +4,8 @@ import com.bitRoute.entity.Questions;
 import com.bitRoute.entity.Scores;
 import com.bitRoute.service.questions.QuestionsService;
 import com.bitRoute.service.score.ScoreService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,27 +19,31 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/api")
+@Api(value = "Questions", description = "Questions Api")
 public class QuestionsController {
     @Autowired
     private QuestionsService questionsService;
 
     private final Logger logger= LoggerFactory.getLogger(QuestionsController.class);
-
+    @ApiOperation("Get all questions")
     @GetMapping("/questions")
     public ResponseEntity<String> getQuestions(){
         logger.info("Inside getQuestions");
         logger.info(questionsService.findQuestions());
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        return new ResponseEntity<>(questionsService.findQuestions(), headers, HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(questionsService.findQuestions());
+//        return new ResponseEntity<>(questionsService.findQuestions(), headers, HttpStatus.CREATED);
     }
-
+    @ApiOperation("Get questions by domain Id")
     @GetMapping("/questions/{domain}")
     public ResponseEntity<String> getQuestionsByDomain(@PathVariable String domain) throws JSONException {
         logger.info("Inside getQuestionsByDomain");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        return new ResponseEntity<>(questionsService.findByDomainId(domain), headers, HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(questionsService.findByDomainId(domain));
+//        return new ResponseEntity<>(questionsService.findByDomainId(domain), headers, HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/bitRoute/controller/ScoreController.java
+++ src/main/java/com/bitRoute/controller/ScoreController.java
@@ -3,6 +3,8 @@ package com.bitRoute.controller;
 import com.bitRoute.entity.Scores;
 import com.bitRoute.service.score.ScoreService;
 //import net.bytebuddy.utility.RandomString;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.management.StringValueExp;
@@ -19,22 +22,26 @@ import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api")
+@Api(value = "Score", description = "Score Api Call")
 public class ScoreController {
     @Autowired
     private ScoreService scoreService;
 
     private final Logger logger= LoggerFactory.getLogger(ScoreController.class);
 
+    @ApiOperation("Post Score")
     @PostMapping("/scores")
     public ResponseEntity<String> saveDepartment(@Valid @RequestBody Scores scores){
         logger.info("Inside saveScore");
         logger.info(scoreService.saveScore(scores).toString());
         // creating UUID
         UUID uid = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
-        scores.setId(String.valueOf(uid.randomUUID()));
+        scores.setId(String.valueOf(UUID.randomUUID()));
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        return new ResponseEntity<>(scoreService.saveScore(scores).toString(), headers, HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(scoreService.saveScore(scores));
+//        return new ResponseEntity<>(scoreService.saveScore(scores).toString(), headers, HttpStatus.CREATED);
     }
 
 }
